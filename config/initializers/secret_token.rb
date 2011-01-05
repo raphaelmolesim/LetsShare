4efcaa6c350330813fc3a665cf56f9b170de3ce5
Raphael Molesim <raphael.sm86@gmail.com>
2011-01-05 23:34:32 +0000

    Started Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetsShare::Application.config.secret_token = '9f56fc7674d936d69fc0f25d6a03a69bd08ef1bc1727246a58f2f11e0e62cc3f6cb42ae1abffc35b033364915286f613b8cad492fb42d144022629a32e18cd73'
