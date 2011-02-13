3fce2a941be529f22ed8b04724dbe9dceeaadd7f
Raphael Molesim <raphael.sm86@gmail.com>
2011-02-13 12:57:55 +0000

    Setting the facebook token when create an user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :except => [ :create ]
   
-  def index
-    @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
-  end
-
   def show
     @user = User.find(params[:id])
 
@@ -18,39 +9,11 @@ class UsersController < ApplicationController
       format.xml  { render :xml => @user }
     end
   end
-
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user }
-    end
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
+  
   def create
     @user = User.get params[:user][:facebook_token]
     @user.save
     session[:facebook_token] = @user.facebook_token
     redirect_to @user
   end
-
-  def update
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,15 @@ class User < ActiveRecord::Base
     user_info = Facebook.get_user_info facebook_token
     user = User.find_by_facebook_id user_info["id"]
     return user if not user.nil?
-    User.new user_info
+    User.new user_info, facebook_token
   end
   
-  def self.new json_data
+  def self.new json_data, token
     user = User.new
     attributes = user.attributes.keys.find_all { |attr| facebook_attribute? attr }
     attributes.each { |attr| eval "user.#{attr} = json_data[\"#{attr}\"]" }
     user.facebook_id = json_data["facebook_id"]
+    user.facebook_token = token
     user
   end
   
